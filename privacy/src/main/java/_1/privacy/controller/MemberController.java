44c39f681dc536ae6af3b095b1f2c618b4690d82
모험가 <80528044+mingi1123@users.noreply.github.com>
2023-05-16 18:47:04 +0900

    Update: CrossOrigin

--- privacy/src/main/java/_1/privacy/controller/MemberController.java
+++ privacy/src/main/java/_1/privacy/controller/MemberController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class MemberController {
 
     private final MemberService memberService;
